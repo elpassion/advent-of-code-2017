1f0a99ab2b518280d0d43963ec17e09f96eef592
ghostbuster91 <kghost0@gmail.com>
2017-12-08 13:02:02 +0100

    Add case for circualr list

--- task01/Task01Test.py
+++ task01/Task01Test.py
@@ -6,6 +6,8 @@ def sumOfMatchingFollowers(input):
     for ndx, member in enumerate(input):
         if ndx < len(input) - 1 and input[ndx] == input[ndx + 1]:
             matched.append(int(member))
+        if ndx == len(input) - 1 and input[ndx] == input[0]:
+            matched.append(int(member))
     return sum(matched)
 
 
@@ -13,9 +15,13 @@ class Task01Test(unittest.TestCase):
     def setUp(self):
         self.seq = range(10)
 
-    def test_first_example(self):
+    def test_sample_case(self):
         self.input = "1122"
         self.assertEqual(3, sumOfMatchingFollowers(self.input))
+
+    def test_algorithm_circularity(self):
+        self.input = "1111"
+        self.assertEqual(4, sumOfMatchingFollowers(self.input))
 
 
 if __name__ == '__main__':
