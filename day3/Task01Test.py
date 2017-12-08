55a6975d6cdea2ecaac5b20e0f42bf40f7a5e115
ghostbuster91 <kghost0@gmail.com>
2017-12-08 17:00:19 +0100

    Day03 task01

---
+++ day3/Task01Test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class Task01Test(unittest.TestCase):
+
+    def testA(self):
+        print("I used Arithmetic progression to solve it")
+
+
+if __name__ == '__main__':
+    unittest.main()
