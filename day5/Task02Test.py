494ed64676b5cd3ff93f7860e9edcd56b6a253f0
ghostbuster91 <kghost0@gmail.com>
2017-12-26 12:08:57 +0100

    D5T2

---
+++ day5/Task02Test.py
@@ -0,0 +1,67 @@
+import unittest
+
+
+class Task02Test(unittest.TestCase):
+
+    def test_count_steps(self):
+        with open("input.txt") as f:
+            input = f.readlines()
+        input = [int(x.strip()) for x in input]
+        current = 0
+        length = len(input)
+        count = 0
+        while True:
+            current = next_step(input, current)
+            count = count + 1
+            if current >= length:
+                break
+        print(count)
+
+    def test_first_step(self):
+        input = [0, 3, 0, 1, -3]
+        current = 0
+        current = next_step(input, current)
+        self.assertEqual(0, current)
+        self.assertEqual([1, 3, 0, 1, -3], input)
+
+    def test_second_step(self):
+        input = [1, 3, 0, 1, -3]
+        current = 0
+        current = next_step(input, current)
+        self.assertEqual(1, current)
+        self.assertEqual([2, 3, 0, 1, -3], input)
+
+    def test_third_step(self):
+        input = [2, 3, 0, 1, -3]
+        current = 1
+        current = next_step(input, current)
+        self.assertEqual(4, current)
+        self.assertEqual([2, 2, 0, 1, -3], input)
+
+    def test_fourth_step(self):
+        input = [2, 2, 0, 1, -3]
+        current = 4
+        current = next_step(input, current)
+        self.assertEqual(1, current)
+        self.assertEqual([2, 2, 0, 1, -2], input)
+
+    def test_fifth_step(self):
+        input = [2, 2, 0, 1, -2]
+        current = 1
+        current = next_step(input, current)
+        self.assertEqual(3, current)
+        self.assertEqual([2, 3, 0, 1, -2], input)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+def next_step(input, current):
+    old = current
+    current = current + input[current]
+    if input[old] >= 3:
+        input[old] = input[old] - 1
+    else:
+        input[old] = input[old] + 1
+    return current
