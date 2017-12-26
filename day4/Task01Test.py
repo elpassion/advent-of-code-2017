5bcbd95adab16a68caeaf1fa9267d4b7e85e6f38
ghostbuster91 <kghost0@gmail.com>
2017-12-26 10:50:53 +0100

    d4t1

---
+++ day4/Task01Test.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+class Task01Test(unittest.TestCase):
+
+    def test_count_valid_passphrases(self):
+        with open("Task01Input.txt") as f:
+            content = f.readlines()
+        content = [x.strip() for x in content]
+        count = len(filter(lambda x: is_valid(x), content))
+        print(count)
+
+    def test_simple_valid_passphrase(self):
+        self.assertTrue(is_valid("aa"))
+
+    def test_passphrase_with_duplicates_is_invalid(self):
+        self.assertFalse(is_valid("aa aa"))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+def is_valid(passphrase):
+    chunks = passphrase.split()
+    return len(set(chunks)) == len(chunks)
