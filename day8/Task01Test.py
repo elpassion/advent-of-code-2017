906c767fedd63ea34eefbb125dd5f25abdc7236a
ghostbuster91 <kghost0@gmail.com>
2018-01-06 18:21:23 +0100

    D08T01 evaluating contition

--- day8/Task01Test.py
+++ day8/Task01Test.py
@@ -28,9 +28,27 @@ class Task01Test(unittest.TestCase):
         line = "a inc 10 if a > 1"
         self.assertEqual("1", parse(line)['condition_value'])
 
+    def test_execute_line(self):
+        execution_info = parse("a inc 10 if a < 1")
+        registers = dict()
+        execute(registers, execution_info)
+        self.assertEqual(10, registers['a'])
+
+    def test_evaluate_false_condition(self):
+        execution_info = parse("a inc 10 if a > 1")
+        registers = dict()
+        result = evalute_condition(registers, execution_info)
+        self.assertFalse(result)
+
+    def test_evaluate_true_condition(self):
+        execution_info = parse("a inc 10 if a < 1")
+        registers = dict()
+        result = evalute_condition(registers, execution_info)
+        self.assertTrue(result)
+
 
 def parse(line):
-    parsed_line = re.search("([a-z]) (inc|dec) (-?\d+) if ([a-z]) (>|<|!=|==|>=|<=) (-?\d+)", line)
+    parsed_line = re.search("([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) (>|<|!=|==|>=|<=) (-?\d+)", line)
     return {
         'operation_register': parsed_line.group(1),
         'operation': parsed_line.group(2),
@@ -39,6 +57,25 @@ def parse(line):
         'condition': parsed_line.group(5),
         'condition_value': parsed_line.group(6)
     }
+
+
+def execute(registers, execution_info):
+    register = execution_info['operation_register']
+    register_value = registers.get(register, 0)
+    operation = execution_info['operation']
+    if operation == 'inc':
+        register_value += int(execution_info['operation_value'])
+    else:
+        register_value -= int(execution_info['operation_value'])
+    registers[register] = register_value
+    return registers
+
+
+def evalute_condition(registers, execution_info):
+    condition_register = execution_info['condition_register']
+    condition_value = execution_info['condition_value']
+    register_value = str(registers.get(condition_register, 0))
+    return eval(register_value + execution_info['condition'] + condition_value)
 
 
 if __name__ == '__main__':
